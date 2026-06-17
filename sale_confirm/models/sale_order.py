5da19902e276ea6acf7a731555badbea7b36fa13
sayanth1357 <sayanthksayanthk@gmail.com>
2026-06-17 13:03:47 +0530

    17-jun

--- sale_confirm/models/sale_order.py
+++ sale_confirm/models/sale_order.py
@@ -1,69 +1,31 @@
+from docutils.nodes import line
+
 from odoo import fields,models
-
-
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
 
     def action_confirm(self):
-        print(123,self)
-        print(543,self.order_line)
-        prod=[]
-        for product in self.order_line:
-            print(product)
-            print(654,product.product_template_id)
-            print(765,product.product_uom_qty)
-            if product.product_template_id not in prod:
-                prod.append(product.product_template_id)
-                print(987654,prod)
 
+        products={}
+        for line in self.order_line:
+            product=line.product_id.id
+            print(23456,product)
+            if product  in products:
+                products[product].product_uom_qty+=line.product_uom_qty
+                # products[product].price_unit +=line.price_unit
+                products[product].price_unit*=products[product].product_uom_qty
+                products[product].price_unit=(products[product].price_unit/ products[product].product_uom_qty)
+                # products[product].price_subtotal=( products[product].price_unit * products[product].product_uom_qty)
+                # products[product].price_subtotal+=products[product].price_unit
+                # print( 1234,products[product].price_subtotal)
+                # products[product].price_unit=(products[product].price_unit/products[product].product_uom_qty)
+
+                line.unlink()
+            else:
+                products[product]=line
+                print(998877,products[product])
         super().action_confirm()
-
-
-
-
-
-
-            # prod=[]
-            # for prod2 in self.order_line:
-            #     if product.product_template_id==prod2.product_template_id:
-            #         prod.append(prod2)
-            #         print( prod.append(prod2))
-            #         qty=product.product_uom_qty+prod2.product_uom_qty
-            #         self.order_line=[(Command.create({
-            #             'product_template_id':prod2.id,
-            #             'product_uom_qty':qty,
-            #         }))]
-            #     else:
-            #         super().action_confirm()
-
-
-
-            
-        # for order in self:
-        #     prod=[]
-        #     for product in order.order_line:
-        #         if product.product_template_id.id not in prod and product.price_unit:
-        #             prod.append(product.product_template_id.id)
-        #             qty=product.product_uom_qty+product.product_uom_qty
-                # if product.product_template_id not in prod:
-                #      prod.append(product.product_template_id)
-                #      for other_prod in order.order_line:
-
-
-
-            #          line = self.order_line.filtered(lambda x: x.product_template_id.id == product.id)
-            #          if line:
-            #              qty=product.product_uom_qty+product.product_uom_qty
-            #              self.order_line = [(Command.create({
-            #              'product_id': product.id,
-            #              'product_qty': qty,
-            #          }))]
-            # super().action_confirm()
-            #
-
-
-
 
 
